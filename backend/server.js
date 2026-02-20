04ba383042b52b5fd44eb31d4589e8386d2eab7c
legacyceo94-prog <legacyceo94@gmail.com>
2026-02-20 15:37:27 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"]
 }));
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Database Connection
